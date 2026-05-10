58ee5df3a591b63ba583afcbd53a391d8c8ffaeb
usmanlatif85 <u208365@gmail.com>
2026-05-10 15:46:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,57 @@
-// User se age input lo aur check karo:
+1)User se age input lo aur check karo:
 
-// agar age 18 ya us se zyada ho → "Adult"
-// warna → "Minor"
-// let age = prompt("Please enter your age:");
-// if (age >= 18 ) {
-//     console.log("Adult");
-// } else{
-//     console.log("Minor");
-// }
-// Do numbers lo aur:
+agar age 18 ya us se zyada ho → "Adult"
+warna → "Minor"
+let age = prompt("Please enter your age:");
+if (age >= 18 ) {
+    console.log("Adult");
+} else{
+    console.log("Minor");
+}
 
-// sum
-// subtraction
-// multiplication
-// division print karo
-// let num1 = parseInt(prompt("Enter first number:"));
-// let num2 = parseInt(prompt("Enter second number:"));
-// let sum = num1 + num2;
-// let difference = num1 - num2;
-// let product = num1 * num2;  
-// let quotient = num1 / num2;
-// console.log("Sum: " + sum);
-// console.log("Difference: " + difference);
-// console.log("Product: " + product);
-// console.log("Quotient: " + quotient);
-// Check karo number even hai ya odd.
-// let number = parseInt(prompt("Enter a number:"));
-// if (number % 2===0){
-//     console.log("Even");
-// } else if (number % 2 !== 0){
-//     console.log("Odd");
-// }else {
-//     console.log("Invalid input");
-// }
-//  1 se 50 tak sirf even numbers print karo.
-// let limit = 50;
-// for (let i=1; i<=50; i++){
-//     if (i % 2 !== 0){
-//         console.log(i);
-//     }
-// }
-// User se number lo aur us ka table print karo (1–10).
-// let number = parseInt(prompt("Enter a number:")); 
-// for (let i = 1; i<=10; i++){
-//     let result = number * i;
-//     console.log(number + " x " + i + " = " + result);
-// }
+
+2)Do numbers lo aur:
+
+sum
+subtraction
+multiplication
+division print karo
+let num1 = parseInt(prompt("Enter first number:"));
+let num2 = parseInt(prompt("Enter second number:"));
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;  
+let quotient = num1 / num2;
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
+
+
+3)Check karo number even hai ya odd.
+let number = parseInt(prompt("Enter a number:"));
+if (number % 2===0){
+    console.log("Even");
+} else if (number % 2 !== 0){
+    console.log("Odd");
+}else {
+    console.log("Invalid input");
+}
+
+
+4) 1 se 50 tak sirf even numbers print karo.
+let limit = 50;
+for (let i=1; i<=50; i++){
+    if (i % 2 !== 0){
+        console.log(i);
+    }
+}
+
+
+5)User se number lo aur us ka table print karo (1–10).
+let number = parseInt(prompt("Enter a number:")); 
+for (let i = 1; i<=10; i++){
+    let result = number * i;
+    console.log(number + " x " + i + " = " + result);
+}
 
